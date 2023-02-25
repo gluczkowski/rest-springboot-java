141d910b80aaf38645f5a5935d0ba45d0221dda9
Matheus <matheus@lexp.com.br>
2023-02-25 16:47:23 -0300

    First commit

---
+++ rest-springboot-java19/src/main/java/br/com/gluczkowski/restspringbootjava19/RestSpringbootJava19Application.java
@@ -0,0 +1,13 @@
+package br.com.gluczkowski.restspringbootjava19;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSpringbootJava19Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSpringbootJava19Application.class, args);
+	}
+
+}
---
+++ rest-springboot-java19/src/test/java/br/com/gluczkowski/restspringbootjava19/RestSpringbootJava19ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gluczkowski.restspringbootjava19;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringbootJava19ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
